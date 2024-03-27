db1b4fc9f4973c99ecea7774c76f028cc6bf8fa6
johngaynor <86327580+johngaynor@users.noreply.github.com>
2024-03-27 09:06:08 -0400

    imported types for clerk

--- src/middleware.ts
+++ src/middleware.ts
@@ -25,6 +25,6 @@ export default authMiddleware({
   },
 });
 
-// export const config = {
-//   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-// };
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
