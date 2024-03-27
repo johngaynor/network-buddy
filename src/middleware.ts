aed1e8f44a25156936d5ffcc88369b432ef31f61
johngaynor <86327580+johngaynor@users.noreply.github.com>
2024-03-27 09:12:52 -0400

    tried adding return type to redirectToSignIn

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,7 +10,7 @@ export default authMiddleware({
     console.log(auth?.userId);
     // Handle users who aren't authenticated
     if (!auth.userId && !auth.isPublicRoute) {
-      return redirectToSignIn({ returnBackUrl: req.url });
+      return redirectToSignIn({ returnBackUrl: req.url }) as NextResponse;
     }
     // Redirect signed in users to organization selection page if they are not active in an organization
     // if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/") {
