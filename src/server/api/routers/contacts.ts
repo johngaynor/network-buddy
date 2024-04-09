56fb32956ed324620200ba92d90d58f3b19ac1f4
johngaynor <86327580+johngaynor@users.noreply.github.com>
2024-04-08 20:55:25 -0400

    final simplification of data

--- src/server/api/routers/contacts.ts
+++ src/server/api/routers/contacts.ts
@@ -9,35 +9,6 @@ export const contactsRouter = createTRPCRouter({
       //   userId: '32352342'
       // },
       include: {
-        Affiliation: {
-          // take: 1, // use take to only return the first row. not a problem now but might be later when the data gets big
-          select: {
-            affiliation: true,
-            updatedAt: true,
-          },
-          orderBy: {
-            updatedAt: "desc",
-          },
-        },
-        Occupation: {
-          select: {
-            company: true,
-            position: true,
-            updatedAt: true,
-          },
-          orderBy: {
-            updatedAt: "desc",
-          },
-        },
-        Notes: {
-          select: {
-            notes: true,
-            updatedAt: true,
-          },
-          orderBy: {
-            updatedAt: "desc",
-          },
-        },
         Interactions: {
           select: {
             title: true,
@@ -57,14 +28,14 @@ export const contactsRouter = createTRPCRouter({
       },
     });
   }),
-  newContact: publicProcedure.query(({ ctx }) => {
-    return ctx.db.contact.create({
-      data: {
-        userId: "...",
-        name: "TEST CREATE",
-      },
-    });
-  }),
+  // newContact: publicProcedure.query(({ ctx }) => {
+  //   return ctx.db.contact.create({
+  //     data: {
+  //       userId: "...",
+  //       name: "TEST CREATE",
+  //     },
+  //   });
+  // }),
   // updateAffiliation: publicProcedure.query(({ ctx }) => {
   //   return ctx.db.contact.update({
   //     where: {
