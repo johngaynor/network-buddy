7eed7271d3e9be0352fdb0643d47cd22bc5c6296
johngaynor <86327580+johngaynor@users.noreply.github.com>
2024-04-14 19:15:42 -0400

    final styling for add modal

--- src/components/contacts/AddModal.tsx
+++ src/components/contacts/AddModal.tsx
@@ -27,6 +27,7 @@ export const AddModal = (props: {
 
   const { mutate, isPending } = api.contacts.newContact.useMutation({
     onSuccess: () => {
+      toast.success(`Successfully added ${formData.name} as a contact!`);
       setFormData({
         name: "",
         affiliation: "",
@@ -70,18 +71,20 @@ export const AddModal = (props: {
           <div className="relative flex w-full flex-col rounded-lg border-0 bg-white shadow-lg outline-none focus:outline-none">
             {/*header*/}
             <div className="flex items-center justify-center rounded-t p-5">
-              <h3 className="text-3xl font-semibold">Add Contact</h3>
+              <h3 className="text-3xl">Add Contact</h3>
             </div>
             {/*body*/}
             <div className="relative flex flex-col p-6">
               {/* first row of inputs */}
-              <div className="flex w-full flex-row">
+              <div
+                className={`flex w-full flex-row ${formErrors.name || formErrors.affiliation ? "pb-2" : "pb-7"}`}
+              >
                 <div className="flex w-1/2 flex-col pr-3">
                   <label>Name</label>
                   <input
-                    className="mt-1 rounded-lg border-2 p-3"
+                    className="mt-1 rounded-lg border-2 p-2"
                     placeholder="Joe Smith"
-                    id="name1"
+                    id="name"
                     required
                     onChange={(e) =>
                       setFormData({ ...formData, name: e.target.value })
@@ -93,60 +96,83 @@ export const AddModal = (props: {
                   ) : null}
                 </div>
                 <div className="flex w-1/2 flex-col pl-3">
-                  <label>Name</label>
+                  <label>Affiliation</label>
                   <input
-                    className="mt-1 rounded-lg border-2 p-3"
-                    placeholder="Joe Smith"
-                    id="name1"
+                    className="mt-1 rounded-lg border-2 p-2"
+                    placeholder="Mutual friend of ___, etc."
+                    id="affiliation"
                     required
                     onChange={(e) =>
-                      setFormData({ ...formData, name: e.target.value })
+                      setFormData({ ...formData, affiliation: e.target.value })
                     }
-                    value={formData.name}
+                    value={formData.affiliation}
                   />
-                  {formErrors.name ? (
-                    <p className="text-red-500">{formErrors.name}</p>
+                  {formErrors.affiliation ? (
+                    <p className="text-red-500">{formErrors.affiliation}</p>
                   ) : null}
                 </div>
               </div>
               {/* second row of inputs */}
-              <div className="flex w-full flex-row">
+              <div
+                className={`flex w-full flex-row ${formErrors.position || formErrors.company ? "pb-2" : "pb-7"}`}
+              >
                 <div className="flex w-1/2 flex-col pr-3">
-                  <label>Name</label>
+                  <label>Company</label>
                   <input
-                    className="mt-1 rounded-lg border-2 p-3"
-                    placeholder="Joe Smith"
-                    id="name1"
+                    className="mt-1 rounded-lg border-2 p-2"
+                    placeholder="Microsoft"
+                    id="company"
                     required
                     onChange={(e) =>
-                      setFormData({ ...formData, name: e.target.value })
+                      setFormData({ ...formData, company: e.target.value })
                     }
-                    value={formData.name}
+                    value={formData.company}
                   />
-                  {formErrors.name ? (
-                    <p className="text-red-500">{formErrors.name}</p>
+                  {formErrors.company ? (
+                    <p className="text-red-500">{formErrors.company}</p>
                   ) : null}
                 </div>
                 <div className="flex w-1/2 flex-col pl-3">
-                  <label>Name</label>
+                  <label>Position</label>
                   <input
-                    className="mt-1 rounded-lg border-2 p-3"
-                    placeholder="Joe Smith"
-                    id="name1"
+                    className="mt-1 rounded-lg border-2 p-2"
+                    placeholder="Software Developer"
+                    id="position"
                     required
                     onChange={(e) =>
-                      setFormData({ ...formData, name: e.target.value })
+                      setFormData({ ...formData, position: e.target.value })
                     }
-                    value={formData.name}
+                    value={formData.position}
                   />
-                  {formErrors.name ? (
-                    <p className="text-red-500">{formErrors.name}</p>
+                  {formErrors.position ? (
+                    <p className="text-red-500">{formErrors.position}</p>
+                  ) : null}
+                </div>
+              </div>
+              {/* third row of inputs */}
+              <div
+                className={`flex w-full flex-row ${formErrors.notes ? "pb-2" : "pb-7"}`}
+              >
+                <div className="flex w-full flex-col">
+                  <label>Notes</label>
+                  <textarea
+                    className="mt-1 h-40 resize-none rounded-lg border-2 p-2"
+                    placeholder="General notes... i.e. where are they from?"
+                    id="notes"
+                    required
+                    onChange={(e) =>
+                      setFormData({ ...formData, notes: e.target.value })
+                    }
+                    value={formData.notes}
+                  />
+                  {formErrors.notes ? (
+                    <p className="text-red-500">{formErrors.notes}</p>
                   ) : null}
                 </div>
               </div>
             </div>
             {/*footer*/}
-            <div className="border-blueGray-200 flex items-center justify-end rounded-b border-t border-solid p-6">
+            <div className="flex items-center justify-end rounded-b p-6">
               <button
                 className="background-transparent mb-1 mr-1 px-6 py-2 text-sm font-bold uppercase text-red-500 outline-none transition-all duration-150 ease-linear focus:outline-none"
                 type="button"
@@ -155,7 +181,7 @@ export const AddModal = (props: {
                 Close
               </button>
               <button
-                className="mb-1 mr-1 rounded bg-emerald-500 px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
+                className="mb-1 mr-1 rounded bg-site-blue-r px-6 py-3 text-sm font-bold uppercase text-white shadow outline-none transition-all duration-150 ease-linear hover:shadow-lg focus:outline-none active:bg-emerald-600"
                 type="button"
                 onClick={handleSubmit}
                 disabled={isPending}
