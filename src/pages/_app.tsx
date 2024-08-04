0e4e22af0579891bf11ef021fc2bda823fb1a826
johngaynor <86327580+johngaynor@users.noreply.github.com>
2024-08-04 07:24:24 -0500

    added speed insights

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,12 +5,15 @@ import Layout from "~/components/layout";
 
 import { api } from "~/utils/api";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
       <Layout>
+        <SpeedInsights />
         <Toaster />
         <Component {...pageProps} />
       </Layout>
